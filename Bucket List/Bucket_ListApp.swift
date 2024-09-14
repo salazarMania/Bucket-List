145cb98693865afd5c28fbd1f27c3e478966b5fe
saniya <saniyaaaaakk@gmail.con>
2024-09-14 21:56:55 +0530

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by SANIYA KHATARKAR on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bucket List/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bucket List
+//
+//  Created by SANIYA KHATARKAR on 14/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
